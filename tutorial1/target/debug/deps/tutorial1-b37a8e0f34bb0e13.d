ed6edf2ec85bfa630404c433913742e318f306d0
StrawberryFields0 <stararmani04@gmail.com>
2023-07-06 07:18:21 -0400

    Changes to be committed: 	modified:   tutorial1/target/.rustc_info.json 	new file:   tutorial1/target/debug/.fingerprint/tutorial1-b37a8e0f34bb0e13/dep-test-bin-tutorial1 	new file:   tutorial1/target/debug/.fingerprint/tutorial1-b37a8e0f34bb0e13/invoked.timestamp 	new file:   tutorial1/target/debug/.fingerprint/tutorial1-b37a8e0f34bb0e13/test-bin-tutorial1 	new file:   tutorial1/target/debug/.fingerprint/tutorial1-b37a8e0f34bb0e13/test-bin-tutorial1.json 	new file:   tutorial1/target/debug/deps/libtutorial1-b37a8e0f34bb0e13.rmeta 	new file:   tutorial1/target/debug/deps/tutorial1-b37a8e0f34bb0e13.d 	new file:   tutorial1/target/debug/incremental/tutorial1-34pv78r24sfr4/s-gmknjq7s4g-kfqyp0-33a2w2xyie1ga/dep-graph.bin 	new file:   tutorial1/target/debug/incremental/tutorial1-34pv78r24sfr4/s-gmknjq7s4g-kfqyp0-33a2w2xyie1ga/query-cache.bin 	new file:   tutorial1/target/debug/incremental/tutorial1-34pv78r24sfr4/s-gmknjq7s4g-kfqyp0-33a2w2xyie1ga/work-products.bin 	new file:   tutorial1/target/debug/incremental/tutorial1-34pv78r24sfr4/s-gmknjq7s4g-kfqyp0.lock

---
+++ tutorial1/target/debug/deps/tutorial1-b37a8e0f34bb0e13.d
@@ -0,0 +1,5 @@
+C:\Users\stara\Documents\LEARN-RUST\tutorial1\target\debug\deps\tutorial1-b37a8e0f34bb0e13.rmeta: src\main.rs
+
+C:\Users\stara\Documents\LEARN-RUST\tutorial1\target\debug\deps\tutorial1-b37a8e0f34bb0e13.d: src\main.rs
+
+src\main.rs:
